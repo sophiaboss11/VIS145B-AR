37a0ff3e0df38c5a91764eb7ef3d6d59ac031737
sophiaboss11 <bosssophiaboss@gmail.com>
2020-04-19 14:41:07 -0700

    ar project no build

---
+++ Assets/_Art/Mandala_scene/_scripts/depth_mask.cs
@@ -0,0 +1,40 @@
+﻿/*
+ 
+	Sets the RenderQueue of an object's materials on Awake. This will instance
+	the materials, so the script won't interfere with other renderers that
+	reference the same materials.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class depth_mask : MonoBehaviour
+{
+    [SerializeField]
+    protected int[] m_queues = new int[] { 3000 };
+
+    protected void Awake()
+    {
+        Material[] materials = GetComponent<Renderer>().materials;
+        for (int i = 0; i < materials.Length && i < m_queues.Length; ++i)
+        {
+            materials[i].renderQueue = m_queues[i];
+        }
+    }
+
+
+/*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+}
